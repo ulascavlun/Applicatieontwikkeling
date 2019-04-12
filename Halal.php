5a1e4d74b8ee2201eb40fdb556356c8a8b4d3db3
Lucas Pedro <Lucas8hk@gmail.com>
2019-04-12 14:31:07 +0200

    Create Halal.php

---
+++ Halal.php
@@ -0,0 +1,25 @@
+
+
+<!doctype html>
+
+
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Naamloos document</title>
+</head>
+
+<body>
+	<?php
+		$voornaam = "Lucas";
+		$achternaam = "Pedro";
+		echo "Welkom {$Firstname} {$Lastname}. Dit is je eerste script";
+	
+		$number1 = 5;
+		$number2 = 7;
+	
+		echo = $number1 + $number2;
+	
+	?>
+</body>
+</html>
